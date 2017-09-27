c0b7bddb6fd66dd0840fe3324989ab6bd817e363
Fabian <Fabian>
2017-09-27 09:52:03 -0500

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'centi-grand.herokuapp.com';
+$config['base_url'] = 'https://centi-grand.herokuapp.com';
 
 /*
 |--------------------------------------------------------------------------
